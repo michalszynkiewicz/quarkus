6a551451c26f87fd3e119c85cc72a338ab269c39
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2019-11-12 11:12:11 +0100

    optional looping through index

---
+++ extensions/security/spi/src/main/java/io/quarkus/security/spi/DenyAllEnabledBuildItem.java
@@ -0,0 +1,9 @@
+package io.quarkus.security.spi;
+
+import io.quarkus.builder.item.MultiBuildItem;
+
+/**
+ * @author Michal Szynkiewicz, michal.l.szynkiewicz@gmail.com
+ */
+public final class DenyAllEnabledBuildItem extends MultiBuildItem {
+}
